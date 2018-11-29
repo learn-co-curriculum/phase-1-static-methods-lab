dfebd33608f74940186c99633c5efe49ddd0e6be
maxwellbenton <maxwellbenton@gmail.com>
2018-11-29 14:33:08 -0500

    drafted solution and tests

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const testVar = {}
+class Formatter {
+  static capitalize( string ) {
+    return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
+  }
 
-function testFunc() {
-  return "hi"
+  static sanitize( string ) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize( sentence ) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let arrayOfWords = sentence.split( " " )
+    for ( let n = 0; n < arrayOfWords.length; n++ ) {
+      if ( n == 0 ) {
+        result.push( this.capitalize( arrayOfWords[ n ] ) )
+      } else {
+        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
+          result.push( arrayOfWords[ n ] )
+        } else {
+          result.push( this.capitalize( arrayOfWords[ n ] ) )
+        }
+      }
+
+    }
+    return result.join( " " );
+  }
+
 }
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,23 +1,45 @@
+describe( "Formatter", () => {
+  describe( "capitalize", () => {
+    it( "is a static method", () => {
+      expect( Formatter.capitalize( "crocodile" ) ).to.not.throw
+    } )
 
+    it( "uppercases the first letter in a String", () => {
+      expect( Formatter.capitalize( "a" ) ).to.equal( "A" )
+      expect( Formatter.capitalize( "alligator" ) ).to.equal( "Alligator" )
+      expect( Formatter.capitalize( "BURSTING BALLOON" ) ).to.equal( "BURSTING BALLOON" )
+    } )
+  } )
 
-describe("index.html", () => {
-  it("includes 'hello'", () => {
-    expect(document.querySelector('body').innerHTML).to.include('hello')
-  })
+  describe( "sanitize", () => {
+    it( "is a static method", () => {
+      expect( Formatter.sanitize( "C catching colds" ) ).to.not.throw
+    } )
 
-  it("includes div", () => {
-    expect(document.querySelector('body').innerHTML, "no empty div element found within body").to.include('<div></div>')
-  })
+    it( "removes non-alphanumeric characters except for dash, single quote and space", () => {
+      expect( Formatter.sanitize( "c" ) ).to.equal( "c" )
+      expect( Formatter.sanitize( "!" ) ).to.equal( "" )
+      expect( Formatter.sanitize( "ca$@#tching cold" ) ).to.equal( "catching cold" )
+      expect( Formatter.sanitize( "Doin' Dishes" ) ).to.equal( "Doin' Dishes" )
+      expect( Formatter.sanitize( "Entert*ain(i{ng-Elephan^ts" ) ).to.equal( "Entertaining-Elephants" )
+    } )
+  } )
 
+  describe( "titleize", () => {
+    it( "is a static method", () => {
+      expect( Formatter.titleize( "F forever foolish" ) ).to.not.throw
+    } )
 
-})
+    it( "capitalizes all words in a setence except 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', and 'from'", () => {
+      expect( Formatter.titleize( "getting giggles" ) ).to.equal( "Getting Giggles" )
+      expect( Formatter.titleize( "where the wild things are" ) ).to.equal( "Where the Wild Things Are" )
+      expect( Formatter.titleize( "chicken soup with rice and a few other songs" ) ).to.equal( "Chicken Soup With Rice and a Few Other Songs" )
+      expect( Formatter.titleize( "Maurice a an but of and for at by from end" ) ).to.equal( "Maurice a an but of and for at by from End" )
+    } )
 
-
-describe("index.js", () => {
-  it("exists", () => {
-    expect(testVar).to.exist
-    expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
-    expect(testFunc).to.exist
-    expect(testFunc()).to.equal('hi')
-  })
-})
+    it( "always capitalizes the first word", () => {
+      expect( Formatter.titleize( "a tale of two cities" ) ).to.equal( "A Tale of Two Cities" )
+      expect( Formatter.titleize( "in the night kitchen" ) ).to.equal( "In the Night Kitchen" )
+    } )
+  } )
+} )
