532e7c67c324621248785a69ef67a7d8bbf4914f
Badr Nasher <badrnasher@gmail.com>
2023-04-07 01:36:46 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^a-z0-9\s'-]/gi, "");
+  }
+  static titleize(string) {
+    const words = string.split(" ");
+    const exceptions = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word;
+      }
+    });
+    return titleizedWords.join(" ");
+  }
 }
